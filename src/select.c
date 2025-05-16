88d756b6f9915ec5a1e5af8fbb08ff71adc7b4e6
Yanis <yanis@Yaniss-MacBook-Air.local>
2025-05-16 15:49:52 +0200

    user input sanitized

--- src/select.c
+++ src/select.c
@@ -1,74 +1,89 @@
 #include "select.h"
-
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "game_state.h"
 #include "print.h"
 
-// TODO regler le probleme des scanf et parser manuellement le chiffre
+char validate(const char range_start, const char range_end) {
+    char option_char[2];
+    int result = 0;
+
+    do {
+        print_text("Votre choix : ");
+        result = scanf("%1s", option_char);
+
+        // flush le reste de la ligne
+        int ch;
+        while ((ch = getchar()) != '\n' && ch != EOF) {}
+
+        if (!result || !isdigit(option_char[0]) || option_char[0] < range_start || option_char[0] > range_end) {
+            print_text("Choix invalide.\n");
+        }
+
+    } while (!result || !isdigit(option_char[0]) || option_char[0] < range_start || option_char[0] > range_end);
+
+    return option_char[0];
+}
+
 StartOption select_option() {
-    int option;
-
     print_text("Choisissez une option:\n\t1. Démarrer une partie\n\t2. Reprendre une partie\n\t3. Quitter\n");
-    print_text("Votre choix : ");
-    // voir https://forums.codeguru.com/showthread.php?329329-When-I-input-chars-in-a-scanf
-    // scanf returns the number of items successfully read
-    if (!scanf("%d", &option)) option = 0;
 
-    while (option < 1 || option > 3) {
-        print_text("Choix invalide: ");
-        if (!scanf("%d", &option)) option = 0;
-    }
+    const char option = validate('1', '3');
 
-    return (StartOption)(option);
+    return (StartOption)(option - '0');
 }
 
 RoundOption select_round_option() {
     print_text("Choisissez une option:\n\t1. Poser une pièce\n\t2. Abandonner\n\t3. Sauvegarder la partie\n");
-    print_text("Votre choix : ");
-    // voir https://forums.codeguru.com/showthread.php?329329-When-I-input-chars-in-a-scanf
-    // scanf returns the number of items successfully read
 
-    int option = getchar() - '0';
+    const char option = validate('1', '3');
 
-    while (option < 1 || option > 3) {
-        if (!isspace(option)) print_text("Choix invalide: ");
-        option = getchar() - '0';
-    }
-
-    return (RoundOption)(option);
+    return (RoundOption)(option - '0');
 }
 
 
 GameMode select_mode() {
-    unsigned int mode;
-
     print_text("Choisissez un mode:\n");
     print_text("\t1. Conquête\n");
     print_text("\t2. Connecte\n");
 
-    print_text("Votre choix : ");
-    if (!scanf("%d", &mode)) mode = 0;
+    const char option = validate('1', '2');
 
-    while (mode < 1 || mode > 2) {
-        print_text("Choix invalide : ");
-        if (!scanf("%d", &mode)) mode = 0;
-    }
-
-    return (GameMode)(mode);
+    return (GameMode)(option - '0');
 }
 
 uint8_t select_dimension() {
-    unsigned int dim;
+    print_text("Choisissez les dimensions de l'échiquier entre 6 et 12.\n");
+    char buffer[3];
+    int value = 0;
 
-    print_text("Entrer les dimensions de l'échiquier: (ex: 8 pour 8x8) ");
-    if (!scanf("%d", &dim)) dim = 0;
+    do {
+        print_text("Votre choix : ");
+        if (scanf("%2s", buffer) != 1) {
+            value = 0;
+        } else {
+            // ensure string contains only digits
+            int valid = 1;
+            for (size_t i = 0; buffer[i] != '\0'; ++i) {
+                if (!isdigit((unsigned char)buffer[i])) {
+                    valid = 0;
+                    break;
+                }
+            }
+            value = valid ? atoi(buffer) : 0;
+        }
 
-    while (dim < 6 || dim > 12) {
-        print_text("La taille doit être comprise entre 6 et 12: ");
-        if (!scanf("%d", &dim)) dim = 0;
-    }
+        // flush remaining input
+        int ch;
+        while ((ch = getchar()) != '\n' && ch != EOF) {}
 
-    return (uint8_t)dim;
+        if (value < 6 || value > 12) {
+            print_text("Choix invalide.\n");
+        }
+
+    } while (value < 6 || value > 12);
+
+    return (uint8_t)value;
 }
